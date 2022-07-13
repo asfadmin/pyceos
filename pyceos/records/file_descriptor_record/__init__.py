b1825a2d5d3a216d2c6d79f5daeb9e53e9de6157
Rohan Weeden <reweeden@alaska.edu>
2022-07-12 16:30:00 -0800

    Add parsing for VolumeDescriptorRecord

--- pyceos/records/file_descriptor_record/__init__.py
+++ pyceos/records/file_descriptor_record/__init__.py
@@ -3,16 +3,19 @@ from construct import GreedyBytes, Switch, this
 from pyceos.enums import FileDescriptorSubtype1
 
 from .sar_leader import SARLeaderFileDescriptorRecord
+from .volume_descriptor import VolumeDescriptorRecord
 
 FileDescriptorRecord = Switch(
     this.header.subtype_1,
     {
         FileDescriptorSubtype1.sar_leader_file_descriptor.name: SARLeaderFileDescriptorRecord,
+        FileDescriptorSubtype1.volume_descriptor.name: VolumeDescriptorRecord,
     },
     default=GreedyBytes
 )
 
 __all__ = (
     "FileDescriptorRecord",
-    "SARLeaderFileDescriptorRecord"
+    "SARLeaderFileDescriptorRecord",
+    "VolumeDescriptorRecord",
 )
